21d72f4c9ac7d93c3b84fc6fb0cf6c44d55afa44
Jasper Ginn <jasperginn@gmail.com>
2024-05-22 17:41:09 +0200

    fix: 0*&0@ logging

--- src/main.py
+++ src/main.py
@@ -1,18 +1,12 @@
-import logging
-
 import google.cloud.logging
 
 client = google.cloud.logging.Client()
+logger = client.logger("dagster-pipes-gcp")
 
-handler = client.get_default_handler()
-logger = logging.getLogger("cloudLogger")
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
-
-__version__ = "0.0.0"
+__version__ = "0.0.0a22+343df2b+acc"
 
 
 def main(request):
-    logger.info(__version__)
-    logger.info("Hello world!")
+    logger.log_struct({"severity": "INFO", "message": __version__})
+    logger.log_struct({"severity": "INFO", "message": "Hello world!"})
     return "boo"
