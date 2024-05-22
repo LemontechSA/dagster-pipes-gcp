ca455aeb8409db08d777302bc3d2f2880872b1d4
Jasper Ginn <jasperginn@gmail.com>
2024-05-22 16:26:25 +0200

    feat: add logging

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
-__version__="0.0.0"
+import logging
+
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+
+client = google.cloud.logging.Client()
+handler = CloudLoggingHandler(client)
+
+logger = logging.getLogger("dagster-pipes-gcp")
+format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(format)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+__version__ = "0.0.0"
 
 
 def main(request):
-    print(__version__)
-    print("Hello world!")
+    logger.info(__version__)
+    logger.info("Hello world!")
     return "boo"
