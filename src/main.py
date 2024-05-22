e4a045d5e56df391b4f6f7267504c820822a4a25
Jasper Ginn <jasperginn@gmail.com>
2024-05-22 17:01:10 +0200

    fix: logging

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
 import logging
 
 import google.cloud.logging
-from google.cloud.logging.handlers import CloudLoggingHandler
+from google.cloud.logging_v2.handlers import CloudLoggingHandler
 
 client = google.cloud.logging.Client()
 handler = CloudLoggingHandler(client)
 
 logger = logging.getLogger("dagster-pipes-gcp")
-format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(format)
-logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 __version__ = "0.0.0"
 
