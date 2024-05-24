a0fb50cad17a036f9b848b2e3b06b0c86b2fe574
Jasper Ginn <jasperginn@gmail.com>
2024-05-24 15:03:47 +0200

    chore: parse from request object

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import flask
 import google.cloud.logging
 
 from .version import __version__
@@ -6,14 +7,14 @@ client = google.cloud.logging.Client()
 logger = client.logger("dagster-pipes-gcp")
 
 
-def main(request):
+def main(request: flask.Request):
     global_log_fields = {}
     trace_header = request.headers.get("X-Cloud-Trace-Context")
     trace = trace_header.split("/")
     global_log_fields["logging.googleapis.com/trace"] = (
         f"projects/jasper-ginn-dagster/traces/{trace[0]}"
     )
-    logger.log_struct({"severity": "INFO", "message": request["name"], **global_log_fields})
+    logger.log_struct({"severity": "INFO", "message": request.json, **global_log_fields})
     logger.log_struct({"severity": "INFO", "message": __version__, **global_log_fields})
     logger.log_struct({"severity": "INFO", "message": "Hello world!", **global_log_fields})
     return "boo"
