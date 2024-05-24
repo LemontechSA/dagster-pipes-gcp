35a3142b2dcb4fe0d720ba00a82f2a7de3907448
Jasper Ginn <jasperginn@gmail.com>
2024-05-24 16:41:34 +0200

    build: add pipes

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
 import flask
-import google.cloud.logging
 
-from .version import __version__
+# import google.cloud.logging
+from dagster_pipes import PipesMappingParamsLoader, open_dagster_pipes
 
-client = google.cloud.logging.Client()
-logger = client.logger("dagster-pipes-gcp")
+# from .version import __version__
+
+# client = google.cloud.logging.Client()
+# logger = client.logger("dagster-pipes-gcp")
 
 
 def main(request: flask.Request):
-    global_log_fields = {}
-    trace_header = request.headers.get("X-Cloud-Trace-Context")
-    trace = trace_header.split("/")
-    global_log_fields["logging.googleapis.com/trace"] = (
-        f"projects/jasper-ginn-dagster/traces/{trace[0]}"
-    )
-    logger.log_struct({"severity": "INFO", "message": request.json, **global_log_fields})
-    logger.log_struct({"severity": "INFO", "message": __version__, **global_log_fields})
-    logger.log_struct({"severity": "INFO", "message": "Hello world!", **global_log_fields})
-    return "boo"
+    event = request.get_json()
+    with open_dagster_pipes(params_loader=PipesMappingParamsLoader(event)) as pipes:
+        event_value = event["some_parameter_value"]
+        pipes.log.info(f"Hello, {event_value}!")
+        pipes.report_asset_materialization(
+            metadata={"some_metric": {"raw_value": event_value + 1, "type": "int"}},
+            data_version="alpha",
+        )
+    return "OK", 200
