234de7d330da2d9531f2e1b9c5cef4a5c72209f6
Jasper Ginn <jasperginn@gmail.com>
2024-05-24 18:07:57 +0200

    chore: add context filter with global python variable

--- src/main.py
+++ src/main.py
@@ -1,35 +1,37 @@
 import logging
-import uuid
+import typing
 
 import flask
 import google.cloud.logging
 from dagster_pipes import PipesMappingParamsLoader, open_dagster_pipes
 from google.cloud.logging.handlers import CloudLoggingHandler
+from google.cloud.logging_v2.handlers import setup_logging
 
-from .version import __version__
+from version import __version__
 
-# from google.cloud.logging_v2.handlers import setup_logging
-
-
-EXECUTION_ID = str(uuid.uuid4())
+EXECUTION_ID: typing.Optional[str] = None
 
 
 class ContextFilter(logging.Filter):
     def filter(self, record):
-        record.execution_id = EXECUTION_ID
+        record.json_fields = {"execution_id": EXECUTION_ID}
         return True
 
 
 client = google.cloud.logging.Client()
 handler = CloudLoggingHandler(client)
 handler.addFilter(ContextFilter())
-client.setup_logging(handler)
+setup_logging(handler)
 
 # client = google.cloud.logging.Client()
 # logger = client.logger("dagster-pipes-gcp")
 
 
 def main(request: flask.Request):
+    trace_header = request.headers.get("X-Cloud-Trace-Context")
+    trace = trace_header.split("/")[0]
+    global EXECUTION_ID
+    EXECUTION_ID = trace
     logging.info(f"Version: {__version__}")
     event = request.get_json()
     with open_dagster_pipes(params_loader=PipesMappingParamsLoader(event)) as pipes:
